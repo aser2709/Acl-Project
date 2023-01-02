234fd66a3b378cc94833030bb6c72b9b74794b54
ahmeddos <ahmedmit54@gmail.com>
2023-01-02 04:38:16 +0200

    videos and progress

--- frontend/src/components/RegisteredSubtitleDetails.js
+++ frontend/src/components/RegisteredSubtitleDetails.js
@@ -1,8 +1,26 @@
 import React from "react";
 import { useNavigate } from 'react-router-dom'
-
-
+import Popup from './Popup';
+import ReactPlayer from 'react-player';
+import { useState } from 'react';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+ var now = 0
 const RegisteredSubtitleDetails = ({subtitle}) =>{
+  const [buttonPopup,setButtonPopup] = useState(false);
+
+  
+const url = `${subtitle.youtube[0].youtubelink}`;
+ 
+  subtitle.now = now
+   const watchVideo = async () => {
+    setButtonPopup(true);
+    subtitle.videoWatched = true
+
+   }
+   if(subtitle.videoWatched){
+    if(now < 100)
+    now+=25
+   }
     
     const handleClick= () =>{
         //console.log(subtitle._id)
@@ -13,14 +31,25 @@ const RegisteredSubtitleDetails = ({subtitle}) =>{
     const handleQuizClick = ()=>{
       localStorage.setItem('courseId',subtitle._id)
       window.location.href=`/subtitlequiz?subtitleId=${subtitle._id}&subtitleName=${subtitle.name}`
+      subtitle.quizTaken = true;
     }
+    if(subtitle.quizTaken){
+      if(now < 100)
+      now+=25
+     }
     return (
         <div className='course-details'>
+           <ProgressBar now={now} label={`${now}%`} />
           <h4 onClick={handleClick}>{subtitle.name}</h4>
           <p><strong>Id: </strong>{subtitle._id}</p>
           <button onClick={handleQuizClick}>Take Quiz</button>
+          <button className="buy-course" onClick={watchVideo}>Watch Video</button>
+
+          <Popup className=".popup-vid" trigger={buttonPopup} setTrigger={setButtonPopup}>
+            <ReactPlayer url={url} controls={true} width={720} height={480}/>
+            </Popup>
         </div>
       )
 }
-
+export var now
 export default RegisteredSubtitleDetails
